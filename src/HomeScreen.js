20176d119a16affd39e23987220e0cf16fbf2b70
MahekKamani <mahekkamani2001@gmail.com>
2022-10-09 21:23:03 +0530

    Created Rows

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,22 @@ import React from 'react'
 import './HomeScreen.css'
 import Nav from './Nav.js'
 import Banner from './Banner.js'
+import requests from './Request'
+import Row from './Row'
 
 function HomeScreen() {
   return (
     <div className='homeScreen'>
         <Nav/>
         <Banner/>
+        <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   )
 }
