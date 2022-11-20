081ab11e4c6367c309c1bc9986a102d98bd1f053
MahekKamani <mahekkamani2001@gmail.com>
2022-11-20 14:28:50 +0530

    Updated the cover image in home screen

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -4,7 +4,11 @@ import './LoginScreen.css'
 function LoginScreen() {
   return (
     <div className='LoginScreen'>
-
+        <div className='login-background'>
+            <img className='logo' src='netflix-logo.png' alt=''/>
+            <button className='signin-button'>Sign In</button>
+            <div className='loginscreen-gradient'/>
+        </div>
     </div>
   )
 }
