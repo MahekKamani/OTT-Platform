4f340a6cd48c699c34e36c9902f0bbd0dd1352f8
MahekKamani <mahekkamani2001@gmail.com>
2022-11-26 22:06:13 +0530

    Added a div

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -13,6 +13,7 @@ function LoginScreen() {
                 <h1>Unlimited films, TV programmes and more.</h1>
                 <h2>Watch anywere. Cancel at any time.</h2>
                 <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                <h3>You can enter</h3>
                 <div className='loginscreen-input'>
                   <form>
                     <input type='email' placeholder='Email Address'/>
