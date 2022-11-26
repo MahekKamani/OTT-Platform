a707f359bf42330ba2d03fc9a8fa785d84f82033
MahekKamani <mahekkamani2001@gmail.com>
2022-11-26 16:01:37 +0530

    Created login body and written 2 headings

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -8,6 +8,12 @@ function LoginScreen() {
             <img className='logo' src='netflix-logo.png' alt=''/>
             <button className='signin-button'>Sign In</button>
             <div className='loginscreen-gradient'/>
+            <div className='loginscreen-body'>
+              <>
+                <h1>Unlimited films, TV programmes and more.</h1>
+                <h2>Watch anywere. Cancel at any time.</h2>
+              </>
+            </div>
         </div>
     </div>
   )
