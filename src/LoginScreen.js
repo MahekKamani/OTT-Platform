a308ff11bdaef58abb21c43483048db460676f15
MahekKamani <mahekkamani2001@gmail.com>
2022-11-26 22:01:33 +0530

    Added all the elements of login form

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -12,6 +12,13 @@ function LoginScreen() {
               <>
                 <h1>Unlimited films, TV programmes and more.</h1>
                 <h2>Watch anywere. Cancel at any time.</h2>
+                <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                <div className='loginscreen-input'>
+                  <form>
+                    <input type='email' placeholder='Email Address'/>
+                    <button className='getstarted-button'>GET STARTED</button>
+                  </form>
+                </div>
               </>
             </div>
         </div>
