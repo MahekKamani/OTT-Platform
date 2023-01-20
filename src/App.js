154e3e19dd410939ba04d4f65002518c74281149
MahekKamani <mahekkamani2001@gmail.com>
2023-01-20 14:30:14 +0530

    Added useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
 import LoginScreen from './LoginScreen';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged((userAuth) => {
+      if(userAuth) {
+
+      } else {
+
+      }
+    });
+
+    return unsubscribe;
+  },[]);
+
   return (
     <div className="App">
       <Router>
